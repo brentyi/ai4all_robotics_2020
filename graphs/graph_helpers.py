de255bd3e70e6961db7c34b869a7d6a36b250eec
Nick Landolfi <nclandolfi@gmail.com>
2020-07-23 16:19:09 -0700

    checkpoint

--- graphs/graph_helpers.py
+++ graphs/graph_helpers.py
@@ -36,7 +36,7 @@ def visualize_adjacency(title, A):
     # Show image
     plt.show()
 
-def visualize_map(title, M, A=None, start=None, end=None, cmap=default_cmap):
+def visualize_map(title, M, A=None, start=None, end=None, path=None, cmap=default_cmap):
     """Visualize a 2D array using matplotlib.
 
     Args:
@@ -49,20 +49,20 @@ def visualize_map(title, M, A=None, start=None, end=None, cmap=default_cmap):
     
     plt.imshow(colored(M, cmap))
     if start is not None:
-        plt.scatter(start[0], start[1], color="blue", label="start")
+        plt.scatter(start[1], start[0], color="blue", label="start")
     if end is not None:
-        plt.scatter(end[0], end[1], color="yellow", label="end")
+        plt.scatter(end[1], end[0], color="yellow", label="end")
+        
+    m, n = M.shape
 
+    def ids(l):
+        p = [l // n, l % n]
+        return p
+
+    def flip(t):
+        return [t[1], t[0]]
         
     if A is not None:
-        m, n = M.shape
-        
-        def ids(l):
-            p = [l // n, l % n]
-            return p
-
-        def flip(t):
-            return [t[1], t[0]]
 
         for i in range(A.shape[0]):
             for j in range(A.shape[1]):
@@ -74,6 +74,16 @@ def visualize_map(title, M, A=None, start=None, end=None, cmap=default_cmap):
                     y = [icoords[1], jcoords[1]]
                     plt.scatter(ids(i)[1], ids(i)[0], color="black")#, label=f"{i}")
                     plt.plot(x, y, color="black", alpha=0.5)#, label=f"{i} -> {j}")
+                   
+    if path is not None:
+        for i in range(len(path)-1):
+            u, v = path[i], path[i+1]
+
+            ucoords = flip(ids(u))
+            vcoords = flip(ids(v))
+            x = [ucoords[0], vcoords[0]]
+            y = [ucoords[1], vcoords[1]]
+            plt.plot(x, y, color="black", alpha=0.5)#, label=f"{i} -> {j}")
                     
     if start or end:
         plt.legend()
@@ -83,7 +93,7 @@ def visualize_map(title, M, A=None, start=None, end=None, cmap=default_cmap):
 
 def visualize_costs(title, M, costs, start=None, end=None):
     m, n = M.shape
-    C = np.zeros_like(M)
+    C = np.zeros(M.shape)
     for i in range(m):
         for j in range(n):
             C[i, j] = costs[i * n + j]
@@ -92,8 +102,9 @@ def visualize_costs(title, M, costs, start=None, end=None):
         plt.scatter(start[1], start[0], color="blue", label="start")
     if end is not None:
         plt.scatter(end[1], end[0], color="yellow", label="end")
-        
+    #np.max([x for x in costs if x != np.inf])
     plt.imshow(C)
+    plt.colorbar()
     if start or end:
         plt.legend()
     plt.title(title)
@@ -118,3 +129,87 @@ def plot_path(M, path):
         x = [ucoords[0], vcoords[0]]
         y = [ucoords[1], vcoords[1]]
         plt.plot(x, y, color="black", alpha=0.5)#, label=f"{i} -> {j}")
+        
+        
+def visualize_path(M, p):
+    # p is the path
+    
+    from IPython.display import HTML
+    from matplotlib.animation import FuncAnimation
+    from matplotlib.patches import Rectangle
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    n = 2
+    T = 5
+    D = 0.4
+
+    def interpolate(init, final):
+        return np.tile(init, (1, T)) + (final - init) * np.linspace(0, 1, T)
+
+    m, n = M.shape
+
+    def ids(l):
+        return [l // m, l % m]
+
+    def flip(t):
+        return [t[1], t[0]]
+
+    ps = np.zeros((2, T * (len(p)-1)))
+    print(ps.shape)
+    for i in range(len(p)-1):
+        u, v = p[i], p[i+1]
+        ucoords = np.array(flip(ids(u))).reshape((2, 1))
+        vcoords = np.array(flip(ids(v))).reshape((2, 1))
+        ps[:, i*T:T*(i+1)] = interpolate(ucoords, vcoords)
+
+
+    # from http://python4econ.blogspot.com/2013/03/matlabs-cylinder-command-in-python.html
+    def cylinder(r, n):
+        """
+        Returns the unit cylinder that corresponds to the curve r.
+        INPUTS:  r - a vector of radii
+                 n - number of coordinates to return for each element in r
+
+        OUTPUTS: x,y,z - coordinates of points
+        """
+
+        # ensure that r is a column vector
+        r = np.atleast_2d(r)
+        r_rows, r_cols = r.shape
+
+        if r_cols > r_rows:
+            r = r.T
+        # find points along x and y axes
+        points = np.linspace(0, 2 * np.pi, n + 1)
+        x = np.cos(points) * r
+        y = np.sin(points) * r
+
+        # find points along z axis
+        rpoints = np.atleast_2d(np.linspace(0, 1, len(r)))
+        z = np.ones((1, n + 1)) * rpoints.T
+
+        return x, y, z
+
+
+    x, y, z = cylinder(D / 2, 100)
+    x = x.squeeze()
+    y = y.squeeze()
+    z = z.squeeze()
+
+    fig, ax = plt.subplots(figsize=(10, 10))
+    plt.close(fig)
+
+    ax.axis("equal")
+    #ax.axis([-1.5, 1.5, -1.5, 1.5])
+    p_data = ax.plot(ps[0, :], ps[1, :], color="C0")[0]
+    ax.scatter(ps[0, :], ps[1, :], color="C0", marker=".")
+
+    ax.imshow(colored(M, default_cmap))
+    def update(i):
+        p_data.set_data(ps[0, i] + x, ps[1, i] + y)
+        ax.set_title(f"i = {i}")
+
+
+    ani = FuncAnimation(fig, update, interval=100, frames=T*(len(p) - 1))
+    HTML(ani.to_html5_video())
